7d6cb40ba96c1cefeee52d024b17482f8fa245ee
Markku Rikola <markku.rikola@futurice.com>
2018-07-13 22:01:04 +0300

    explicitly set library directory too

--- build_windows.sh
+++ build_windows.sh
@@ -29,7 +29,7 @@ function build_sdl_mixer {
 	git clone https://github.com/SDL-mirror/SDL_mixer.git
 	pushd SDL_mixer
 
-	./configure "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32" --host=i686-w64-mingw32 --with-sdl-prefix=${install_dir}/built_sdl --prefix=${install_dir}/built_sdl_mixer
+	./configure "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32 -L${install_dir}/built_sdl/lib" --host=i686-w64-mingw32 --with-sdl-prefix=${install_dir}/built_sdl --prefix=${install_dir}/built_sdl_mixer
 	remove_mwindows
 	make
 	make install
